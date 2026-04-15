f43982242d0d4d77bda7704e77531dba0b0cb1d2
damian10z <Damianzazueta306@gmail.com>
2026-04-15 01:39:25 -0600

    sa

--- src/ejercicioI.java
+++ src/ejercicioI.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class SCA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] autorizados = {1024, 2048, 4096, 8192, 16384};
+        
+        System.out.print("Ingrese ID de credencial: ");
+        int idIngresado = sc.nextInt();
+        int acceso = 0; 
+        for (int i = 0; i < autorizados.length; i++) {
+            if (idIngresado == autorizados[i]) {
+                acceso = 1;
+                break; 
+            }
+        }
+        
+        if (acceso == 1) {
+            System.out.println("[ACCESO CONCEDIDO] Bienvenido al Site.");
+        } else {
+            System.out.println("[ALERTA] Acceso denegado. Personal no autorizado.");
+        }
+        
+        sc.close();
+    }
+}
